19858172df061784c92e014e6916d0a68b1d4875
Maurits van der Schee <maurits@vdschee.nl>
2020-12-02 12:20:36 +0100

    day01

--- day01/part1.rs
+++ day01/part1.rs
@@ -1,21 +1,20 @@
-use std::io::{BufRead, BufReader};
 use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 fn main() {
     let mut vec = Vec::new();
-        
+
     let file = File::open("input").expect("cannot open file");
     let file = BufReader::new(file);
     for line in file.lines() {
-        let s:String = line.ok().unwrap();
+        let s: String = line.ok().unwrap();
         let num: i32 = s.parse().unwrap();
         for v in &vec {
-            if v+num==2020 {
-                println!("{}+{}=2020",v,num);
-                println!("{}*{}={}",v,num,v*num);
+            if v + num == 2020 {
+                println!("{}+{}=2020", v, num);
+                println!("{}*{}={}", v, num, v * num);
             }
         }
         vec.push(num);
     }
-
 }
