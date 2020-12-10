e233864fc2c4e2ca97bd29953afaaedda72a664f
Maurits van der Schee <maurits@vdschee.nl>
2020-12-10 10:27:07 +0100

    day10

--- day10/part2/src/main.rs
+++ day10/part2/src/main.rs
@@ -5,25 +5,25 @@ fn main() {
     let file = File::open("input").expect("cannot open file");
     let file = BufReader::new(file);
     let mut numbers: Vec<i64> = Vec::new();
+    numbers.push(0);
     for line in file.lines() {
         let s = line.unwrap();
         let number = s.parse().unwrap();
         numbers.push(number);
     }
     numbers.sort();
-    let mut used: Vec<i64> = Vec::new();
-    let mut j = 0;
+    numbers.push(numbers[numbers.len()-1]+3);
+    // do clever stuff
+    let mut options:Vec<i64> = vec![0;numbers.len()];
+    options[0] = 1;
     for i in 0..numbers.len() {
-        let n = numbers[i];
-        if n>=j+1 && n<=j+3 {
-            used.push(n);
-            j = n;
+        let mut opts = 0;
+        for o in 1..=3 {
+            if i >= o && numbers[i]-numbers[i-o] <= 3 { 
+                opts += options[i-o];
+            }
         }
+        options[i] += opts;
     }
-    let l = used.len();
-    let options = 1;
-    for i in 0..l {
-        let n = numbers[l-i];
-    }
-    println!("{}", options);
+    println!("{}", options[options.len()-1]);
 }
