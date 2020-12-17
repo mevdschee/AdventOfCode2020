9fa702f81eafa8f563bb52b51038b0219e586c66
Maurits van der Schee <maurits@vdschee.nl>
2020-12-18 00:29:41 +0100

    day11

--- day11/part1/src/main.rs
+++ day11/part1/src/main.rs
@@ -1,45 +1,45 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::collections::HashMap;
 
 fn main() {
     let file = File::open("input").expect("cannot open file");
     let file = BufReader::new(file);
-    let mut seats = HashMap::<(i32,i32),bool>::new();
+    let mut seats = HashMap::new();
     for (y, line) in file.lines().enumerate() {
         for (x, c) in line.unwrap().chars().enumerate() {
-            if c=='L' {
-                seats.insert((x as i32,y as i32), false);
+            if c == 'L' {
+                seats.insert((x as i32, y as i32), false);
             }
         }
     }
     let mut changed = true;
     while changed {
-        let mut next = HashMap::<(i32,i32),bool>::new();
+        let mut next = HashMap::new();
         changed = false;
-        for (key,value) in &seats {
-            let (x,y) = key;
+        for (key, value) in &seats {
+            let (x, y) = key;
             let mut occupied = 0;
             for dx in -1..=1 {
                 for dy in -1..=1 {
-                    if dx==0 && dy ==0 {
+                    if dx == 0 && dy == 0 {
                         continue;
                     }
-                    if seats.contains_key(&(x+dx,y+dy)) {
-                        if seats[&(x+dx,y+dy)] {
+                    if seats.contains_key(&(x + dx, y + dy)) {
+                        if seats[&(x + dx, y + dy)] {
                             occupied += 1;
-                        } 
+                        }
                     }
                 }
             }
             let mut nextvalue = *value;
             if *value {
-                if occupied>=4 {
+                if occupied >= 4 {
                     nextvalue = false;
                     changed = true;
                 }
             } else {
-                if occupied==0 {
+                if occupied == 0 {
                     nextvalue = true;
                     changed = true;
                 }
@@ -48,5 +48,5 @@ fn main() {
         }
         seats = next;
     }
-    println!("{}",seats.values().filter(|&n|*n).count());
+    println!("{}", seats.values().filter(|&n| *n).count());
 }
--- day11/part2/src/main.rs
+++ day11/part2/src/main.rs
@@ -1,44 +1,44 @@
 use std::cmp::max;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::collections::HashMap;
 
 fn main() {
     let file = File::open("input").expect("cannot open file");
     let file = BufReader::new(file);
-    let mut seats = HashMap::<(i32,i32),bool>::new();
-    let mut maxy:i32 = 0;
-    let mut maxx:i32 = 0;
+    let mut seats = HashMap::new();
+    let mut maxy: i32 = 0;
+    let mut maxx: i32 = 0;
     for (y, line) in file.lines().enumerate() {
-        maxy = max(maxy,y as i32);
+        maxy = max(maxy, y as i32);
         for (x, c) in line.unwrap().chars().enumerate() {
-            maxx = max(maxx,x as i32);
-            if c=='L' {
-                seats.insert((x as i32,y as i32), false);
+            maxx = max(maxx, x as i32);
+            if c == 'L' {
+                seats.insert((x as i32, y as i32), false);
             }
         }
     }
-    let size = max(maxx,maxy)+1;
+    let size = max(maxx, maxy) + 1;
     let mut changed = true;
     while changed {
-        let mut next = HashMap::<(i32,i32),bool>::new();
+        let mut next = HashMap::new();
         changed = false;
-        for (key,value) in &seats {
-            let (x,y) = key;
+        for (key, value) in &seats {
+            let (x, y) = key;
             let mut occupied = 0;
             for dx in -1..=1 {
                 for dy in -1..=1 {
-                    if dx==0 && dy ==0 {
+                    if dx == 0 && dy == 0 {
                         continue;
                     }
                     for m in 1..size {
-                        let nx = x+dx*m;
-                        let ny = y+dy*m;
-                        if nx<0 || ny<0 || nx>=size || ny >=size {
+                        let nx = x + dx * m;
+                        let ny = y + dy * m;
+                        if nx < 0 || ny < 0 || nx >= size || ny >= size {
                             break;
                         }
-                        if seats.contains_key(&(nx,ny)) {
-                            if seats[&(nx,ny)] {
+                        if seats.contains_key(&(nx, ny)) {
+                            if seats[&(nx, ny)] {
                                 occupied += 1;
                             }
                             break;
@@ -48,20 +48,19 @@ fn main() {
             }
             let mut nextvalue = *value;
             if *value {
-                if occupied>=5 {
+                if occupied >= 5 {
                     nextvalue = false;
                     changed = true;
                 }
             } else {
-                if occupied==0 {
+                if occupied == 0 {
                     nextvalue = true;
                     changed = true;
                 }
             }
             next.insert(*key, nextvalue);
-                    
         }
         seats = next;
     }
-    println!("{}",seats.values().filter(|&n|*n).count());
+    println!("{}", seats.values().filter(|&n| *n).count());
 }
