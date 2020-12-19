e61134ee8a70f69ab01bc9008dfd07d200ebc37d
Maurits van der Schee <maurits@vdschee.nl>
2020-12-20 00:53:39 +0100

    day19

--- day19/part2/src/main.rs
+++ day19/part2/src/main.rs
@@ -5,9 +5,8 @@ use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
 
-fn make_regex(rules:&HashMap<i32,Vec<Vec<i32>>>, rule:i32, depth: i32) -> String
-{
-    if depth>20 {
+fn make_regex(rules: &HashMap<i32, Vec<Vec<i32>>>, rule: i32, depth: i32, max: i32) -> String {
+    if depth > max {
         return "".to_owned();
     }
     let subrules = &rules[&rule];
@@ -20,15 +19,15 @@ fn make_regex(rules:&HashMap<i32,Vec<Vec<i32>>>, rule:i32, depth: i32) -> String
             } else if *number == -2 {
                 numbers.push("b".to_owned());
             } else {
-                numbers.push(make_regex(rules, *number, depth+1));
+                numbers.push(make_regex(rules, *number, depth + 1, max));
             }
         }
         expressions.push(numbers.join(""));
     }
-    if expressions.len()>1 {
-        return format!("({})",expressions.join("|"));
+    if expressions.len() > 1 {
+        return format!("({})", expressions.join("|"));
     } else {
-        return format!("{}",expressions[0]);
+        return format!("{}", expressions[0]);
     }
 }
 
@@ -44,45 +43,54 @@ fn main() {
     for line in lines {
         let s = line.unwrap();
         if s == "" {
-            part+=1;
+            part += 1;
             continue;
         }
-        if part==1 {
+        if part == 1 {
             let mut parts = s.split(": ");
-            let i:i32 = parts.next().unwrap().parse().unwrap();
+            let i: i32 = parts.next().unwrap().parse().unwrap();
             let subrules = parts.next().unwrap().split(" | ");
             let mut options = vec![];
             for subrule in subrules {
                 let mut option = vec![];
                 for number in subrule.split(" ") {
-                    let n:i32;
+                    let n: i32;
                     if number == "\"a\"" {
                         n = -1;
                     } else if number == "\"b\"" {
                         n = -2;
-                    } else { 
+                    } else {
                         n = number.parse().unwrap();
                     }
                     option.push(n);
                 }
                 options.push(option);
             }
-            rules.insert(i,options);
+            rules.insert(i, options);
         } else {
             messages.insert(s);
         }
     }
     // change rules
-    rules.insert(8,vec![vec![42],vec![42,8]]);
-    rules.insert(11,vec![vec![42,31],vec![42,11,31]]);
+    rules.insert(8, vec![vec![42], vec![42, 8]]);
+    rules.insert(11, vec![vec![42, 31], vec![42, 11, 31]]);
     // evaluate
-    let expression = make_regex(&rules,0,0);
-    let re = Regex::new(&format!("^{}$",&expression)).unwrap();
-    let mut i = 0;
-    for message in messages {
-        if re.is_match(&message) {
-            i += 1;
+    let mut last = 0;
+    let mut depth = 0;
+    loop {
+        let expression = make_regex(&rules, 0, 0, depth);
+        let re = Regex::new(&format!("^{}$", &expression)).unwrap();
+        let mut i = 0;
+        for message in &messages {
+            if re.is_match(&message) {
+                i += 1;
+            }
         }
+        if i > 0 && i == last {
+            break;
+        }
+        last = i;
+        depth += 1;
     }
-    println!("{}",i);
+    println!("{}", last);
 }
