d410f7b9a7b3fa165e5de2e1920fb423cdc53b4f
Maurits van der Schee <maurits@vdschee.nl>
2020-12-23 20:25:53 +0100

    day21

--- day21/part2/src/main.rs
+++ day21/part2/src/main.rs
@@ -1,26 +1,26 @@
-use std::iter::FromIterator;
-use std::collections::HashSet;
 use std::collections::HashMap;
+use std::collections::HashSet;
+use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
-use std::fs::File;
+use std::iter::FromIterator;
 
 fn main() {
     // state
-    let mut ingredient_counts: HashMap<String,i32>= HashMap::new();
-    let mut allergen_ingredients: HashMap<String,HashSet<String>> = HashMap::new();
+    let mut ingredient_counts: HashMap<String, i32> = HashMap::new();
+    let mut allergen_ingredients: HashMap<String, HashSet<String>> = HashMap::new();
     // parse
     let file = File::open("input").expect("cannot open file");
     let file = BufReader::new(file);
     for line in file.lines() {
         let s = line.unwrap().to_owned();
         let mut parts = s.trim_matches(')').split(" (contains ");
-        let mut ingredients:Vec<String> = vec![];
-        for ingredient in parts.next().unwrap().split(" ") {            
+        let mut ingredients: Vec<String> = vec![];
+        for ingredient in parts.next().unwrap().split(" ") {
             ingredients.push(ingredient.to_string());
         }
-        let mut allergens:Vec<String> = vec![];
-        for allergen in parts.next().unwrap().split(", ") {            
+        let mut allergens: Vec<String> = vec![];
+        for allergen in parts.next().unwrap().split(", ") {
             allergens.push(allergen.to_string());
         }
         for a in allergens {
@@ -29,18 +29,31 @@ fn main() {
             } else {
                 let other = HashSet::from_iter(ingredients.to_vec());
                 let intersect = allergen_ingredients[&a].intersection(&other);
-                let set = HashSet::from_iter(intersect.map(|x|x.to_string()));
+                let set = HashSet::from_iter(intersect.map(|x| x.to_string()));
                 allergen_ingredients.insert(a, set);
             }
         }
-        for i in ingredients {            
-            let val = ingredient_counts.get(&i).unwrap_or(&0)+1;
+        for i in ingredients {
+            let val = ingredient_counts.get(&i).unwrap_or(&0) + 1;
             ingredient_counts.insert(i, val);
         }
     }
-    let other = allergen_ingredients.values().fold(HashSet::new(),|a,x| a.into_iter().chain(x).collect());
-    let set: HashSet<&String> = ingredient_counts.keys().collect();
-    let intersect = set.difference(&other);
-    let sum = intersect.fold(0,|a,x|a+ingredient_counts.get(x.to_owned()).unwrap());
-    println!("{:?}", sum);
+    // evaluate
+    let mut mapping = HashMap::new();
+    for _ in 0..allergen_ingredients.len() {
+        for (allergen, ingredients) in &allergen_ingredients {
+            let keys: Vec<String> = mapping.keys().cloned().collect();
+            let removed: HashSet<String> = HashSet::from_iter(keys);
+            let matched: HashSet<String> = ingredients.difference(&removed).cloned().collect();
+            if matched.len() == 1 {
+                let ingredient = matched.iter().next().unwrap();
+                mapping.insert(ingredient.to_string(), allergen);
+            }
+        }
+    }
+    // sort
+    let mut mapping_vect: Vec<(&String, &&String)> = mapping.iter().collect();
+    mapping_vect.sort_by(|a, b| a.1.cmp(b.1));
+    let ingredients: Vec<String> = mapping_vect.into_iter().map(|a| a.0).cloned().collect();
+    println!("{}", ingredients.join(","));
 }
