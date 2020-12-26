a13420fe6a36e5a150525304c3c209212bf547da
Maurits van der Schee <maurits@vdschee.nl>
2020-12-26 08:30:00 +0100

    day22

--- day22/part2/src/main.rs
+++ day22/part2/src/main.rs
@@ -1,7 +1,49 @@
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
 use std::collections::VecDeque;
+
+fn combat(hands: &Vec<VecDeque<i32>>) -> (i32, i32)
+{
+    let mut hands = hands.clone();
+    let mut win = 0;
+    let mut games = HashSet::new();
+    while hands[0].len()>0 && hands[1].len()>0 {
+        let game = format!("{:?}",hands);
+        let card0 = hands[0].pop_front().unwrap();
+        let card1 = hands[1].pop_front().unwrap();
+        //println!("{} vs {} for {:?}",card0, card1, hands);
+        if games.contains(&game) {
+            win = 0;
+        } else {
+            games.insert(game);
+            if (card0 as usize) <= hands[0].len() && (card1 as usize) <= hands[1].len() {
+                let hand0: VecDeque<i32> = hands[0].iter().take(card0 as usize).cloned().collect();
+                let hand1: VecDeque<i32> = hands[1].iter().take(card1 as usize).cloned().collect();
+                let hands = vec![hand0,hand1];
+                win = combat(&hands).0;
+            } else if card0>card1 {
+                win = 0;
+            } else {
+                win = 1;
+            }
+        }
+        if win==0 {
+            hands[0].push_back(card0);
+            hands[0].push_back(card1);
+        } else {
+            hands[1].push_back(card1);
+            hands[1].push_back(card0);
+        }
+    }
+    let mut sum = 0;
+    for i in 1..=hands[win as usize].len() {
+        let number = hands[win as usize].pop_back().unwrap();
+        sum += i as i32 * number;
+    }
+    return (win,sum);
+}
 
 fn main() {
     // state
@@ -24,24 +66,8 @@ fn main() {
         hands[player].push_back(number);
     }
     // evaluate
-    let mut win = 0;
-    while hands[0].len()>0 && hands[1].len()>0 {
-        let card0 = hands[0].pop_front().unwrap();
-        let card1 = hands[1].pop_front().unwrap();
-        if card0>card1 {
-            win = 0;
-            hands[0].push_back(card0);
-            hands[0].push_back(card1);
-        } else {
-            win = 1;
-            hands[1].push_back(card1);
-            hands[1].push_back(card0);
-        }
-    }
-    let mut sum = 0;
-    for i in 1..=hands[win].len() {
-        let number = hands[win].pop_back().unwrap();
-        sum += i as i32 * number;
-    }
+    let sum = combat(&hands).1;
     println!("{}", sum);
 }
+
+// 32873, 30999
