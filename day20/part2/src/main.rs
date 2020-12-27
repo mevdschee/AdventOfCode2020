274a788e3607365def8d2ea00c5e09c81780551a
Maurits van der Schee <maurits@vdschee.nl>
2020-12-27 17:13:21 +0100

    day20

--- day20/part2/src/main.rs
+++ day20/part2/src/main.rs
@@ -2,6 +2,57 @@ use std::collections::HashMap;
 use std::fs::File;
 use std::io::Read;
 
+fn print_tile(tiles: &HashMap<i32, Vec<Vec<i32>>>, tile: i32, rotation: usize) {
+    for n in &tiles[&tile][rotation] {
+        println!("{:#012b}", n);
+    }
+}
+
+fn tile_connects(tiles: &HashMap<i32, Vec<Vec<i32>>>, tile0: i32, tile1: i32) -> i32 {
+    let mut found = false;
+    let mut i = 0;
+    for side0 in &tiles[&tile0] {
+        for side1 in &tiles[&tile1] {
+            if side0[0 as usize] == side1[0 as usize] {
+                found = true;
+                break;
+            }
+            i += 1;
+        }
+        if found {
+            break;
+        }
+    }
+    return i;
+}
+
+fn find_next_tile(
+    tiles: &HashMap<i32, Vec<Vec<i32>>>,
+    image: &Vec<i32>,
+    scores: &HashMap<i32, Vec<i32>>,
+) -> i32 {
+    let y = image.len() / 12;
+    let x = image.len() % 12;
+    let mut pool = 12;
+    if y > 0 && y < 11 {
+        pool += 2;
+    }
+    if x > 0 && x < 11 {
+        pool += 2;
+    }
+    for tile in &scores[&pool] {
+        if image.contains(tile) {
+            continue;
+        }
+        if y == 0 || tile_connects(tiles, *tile, image[(y - 1) * 12 + x]) > 0 {
+            if x == 0 || tile_connects(tiles, *tile, image[y * 12 + x - 1]) > 0 {
+                return *tile;
+            }
+        }
+    }
+    return 0;
+}
+
 fn main() {
     // state
     let mut tiles = HashMap::new();
@@ -12,7 +63,7 @@ fn main() {
     for tile in content.trim().split("\n\n") {
         let mut parts = tile.trim().split(":");
         let header = parts.next().unwrap();
-        let number: i64 = header[5..].parse().unwrap();
+        let number: i32 = header[5..].parse().unwrap();
         let lines = parts.next().unwrap().trim().split("\n");
         let mut image_lines = vec![];
         for line in lines {
@@ -93,11 +144,19 @@ fn main() {
             minimum = score;
         }
         if !scores.contains_key(&score) {
-            scores.insert(score, vec![number]);
+            scores.insert(score, vec![*number]);
         } else {
-            scores.get_mut(&score).unwrap().push(number);
+            scores.get_mut(&score).unwrap().push(*number);
         }
     }
-    let i = scores[&minimum].iter().fold(1, |sum, val| sum * *val);
-    println!("{:?}", i);
+    // build image
+    let mut image = vec![];
+    for _ in 0..144 {
+        image.push(find_next_tile(&tiles, &image, &scores));
+    }
+    for i in 0..2 {
+        println!("{}", image[i]);
+        print_tile(&tiles, image[i], 0);
+    }
+    //println!("{:?}", image);
 }
