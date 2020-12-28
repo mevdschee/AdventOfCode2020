82b07d0003ef5e8980c75877ae3c2343d81a7229
Maurits van der Schee <maurits@vdschee.nl>
2020-12-28 16:57:17 +0100

    day20

--- day20/part2/src/main.rs
+++ day20/part2/src/main.rs
@@ -3,8 +3,10 @@ use std::fs::File;
 use std::io::Read;
 
 fn print_tile(tiles: &HashMap<i32, Vec<Vec<i32>>>, tile: i32, rotation: usize) {
+    println!("{} {}", tile,rotation);
     for n in &tiles[&tile][rotation] {
-        println!("{:#012b}", n);
+        let s = format!("{:#012b}", n);
+        println!("{}",s[2..].to_string().replace("0","..").replace("1","()"));
     }
 }
 
@@ -122,17 +124,23 @@ fn main() {
         images.push(image_reverse_flipped);
 
         let images = vec![
-            images[0].clone(), // 0 normal
-            images[6].clone(), // 90 clock-wise
-            images[3].clone(), // 180 upside down
-            images[5].clone(), // 270 clock-wise
-            images[1].clone(), // 0 mirror (horizontally)
-            images[4].clone(), // 90 clock-wise mirror
-            images[2].clone(), // 180 upside down mirror
-            images[7].clone(), // 270 clock-wise mirror
+            images[0].clone(), // 0 => 0 normal
+            images[6].clone(), // 1 => 90 counter-clock-wise
+            images[3].clone(), // 2 => 180 upside down
+            images[5].clone(), // 3 => 270 counter-clock-wise
+            images[1].clone(), // 4 => 0 mirror (around vertical axis)
+            images[4].clone(), // 5 => 90 counter-clock-wise mirror
+            images[2].clone(), // 6 => 180 upside down mirror
+            images[7].clone(), // 7 => 270 counter-clock-wise mirror
         ];
 
         tiles.insert(number, images);
+        // for i in 0..8{
+        //     print_tile(&tiles,number,i);
+        //     print_tile(&tiles,number,(i+6)%8);
+        //     println!("");
+        // }
+        // panic!("exit");
     }
     // evaluate
     let mut scores = HashMap::new();
@@ -158,20 +166,50 @@ fn main() {
             scores.get_mut(&score).unwrap().push(*number0);
         }
     }
-    let image = &scores[&4][0];
-    for i in 0..8 {
-        if connects.contains_key(&(image,i)) {
-            println!("{:?} -> {:?}",(image,i),connects[&(image,i)]);
-        }
+    //println!("{:?}",connects);
+    let image0 = &scores[&4][0];
+    // first tile orientation
+    let mut rotate = 0;
+    while !connects.contains_key(&(image0,(rotate+1)%4)) || !connects.contains_key(&(image0,(rotate+2)%4)) {
+        rotate+=1;
     }
     // build image
-    let mut image = vec![];
-    for _ in 0..144 {
-        image.push(find_next_tile(&tiles, &image, &scores));
-    }
+    let mut image = vec![image0];
+    let mut rotations = vec![rotate];
+    print_tile(&tiles, *image0, rotate);
+
+    //for y in 0..12 {
+    //    for x in 0..12 {
+            let orientation0 = (rotate+1)%4;
+            let (image1,orientation1) = connects[&(image0,orientation0)];
+            println!("({},{}) {:?} -> {:?}",0,1,(image0,orientation0),(image1,orientation1));
+            /*let mut next_rotate = rotate + match (orientation0, orientation1) {
+                (0,4) => 6,   // 1 = (3 + (8 - 0) + 4 + 6) % 8
+                (1,2) => 7,
+                (1,0) => 5,
+                (1,7) => 2,
+                _ => {
+                    print_tile(&tiles, *image0, rotate);
+                    print_tile(&tiles, *image1, rotate);
+                    panic!("Unknown combination: {:?}", (orientation0, orientation1))
+                },
+            };*/
+            let mut next_rotate = rotate + ( 8 - orientation0) + orientation1 + 6;
+            //if ((8 - orientation0) + orientation1)%2==0 {
+            //    next_rotate += 8;
+            //} else {
+            //    next_rotate += 6;
+            //}
+            next_rotate %= 8;
+            print_tile(&tiles, *image0, rotate);
+            print_tile(&tiles, *image1, next_rotate);
+//        }
+//    }
+    //    image.push(find_next_tile(&tiles, &image, &connects));
+    //}
     /*for i in 0..2 {
         println!("{}", image[i]);
         print_tile(&tiles, image[i], 0);
     }*/
-    println!("{:?}", image);
+    //println!("{:?}", image);
 }
